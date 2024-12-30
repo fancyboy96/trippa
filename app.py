a2febd42104b093336f12a900c31ab215fccc854
jesse.l.kilburn@gmail.com <89227663+fancyboy96@users.noreply.github.com>
2024-12-30 21:57:33 +1030

    finished register route

--- app.py
+++ app.py
@@ -14,6 +14,12 @@ app.secret_key = 'supersecretkey' # REPLACE!!
 #Initialise database
 db = SQLAlchemy(app)
 
+class User(db.Model):  # db.Model is a declarative base class
+    __tablename__ = 'users'  # This explicitly maps the model to the 'users' table
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    hash = db.Column(db.String(200), nullable=False)
+
 
 @app.route("/")
 def index():
@@ -25,11 +31,29 @@ def index():
 def register():
     """Register user"""
     if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        if not username or not password:
+            error = "Must provide a username and password"
+            return render_template("error.html", error=error) 
+
+        existing_user = Users.query.filter_by(username=username).first()
+        if existing_user:
+            error = "User already exists"
+            return render_template("error.html", error=error)
+        
         flash("Registered successfully!")
         return(redirect(url_for("login")))
+
+        # Hash password and add user to the database
+        hashed_password = generate_password_hash(password)
+        new_user = User(username=username, hash=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+
     else:
         return render_template("register.html")
-    return("TODO")
 
 
 @app.route("/login", methods=["GET", "POST"])
